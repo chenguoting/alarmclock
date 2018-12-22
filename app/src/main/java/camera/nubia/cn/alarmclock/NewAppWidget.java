4491208c1c66f74a50665e2f060651d2704c2771
chenguoting <425465257@qq.com>
2018-12-22 14:37:36 +0800

    由于多实例的问题，使用静态变量

--- app/src/main/java/camera/nubia/cn/alarmclock/NewAppWidget.java
+++ app/src/main/java/camera/nubia/cn/alarmclock/NewAppWidget.java
@@ -23,47 +23,49 @@ public class NewAppWidget extends AppWidgetProvider {
     private final static String TAG = "NewAppWidget";
     private final static int requestCode = 1014;
     private final static int REFRESH_INTERVAL = 1000;
-    private AppWidgetManager appWidgetManager;
-    private Context context;
-    private SharedPreferences sharedPreferences;
-    private boolean isInit = false;
-    private RemoteViews views;
-
-    private void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        long cur = System.currentTimeMillis();
-        BeiJingDate date = new BeiJingDate(cur);
-        views.setTextViewText(R.id.appwidget_text, date.getTimeStr());
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, requestCode, new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
-        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
+    private static AppWidgetManager appWidgetManager;
+    private static Context context;
+    private static SharedPreferences sharedPreferences;
+    private static boolean isInit = false;
+    private static RemoteViews views;
+    private static int[][] soundId = new int[][]{
+            {20, 30, R.raw.sound_2030, 0}, //20:30
+            {21, 00, R.raw.sound_2100, 0}, //21:00
+            {21, 30, R.raw.sound_2130, 0}, //21:30
+            {22, 00, R.raw.sound_2200, 0}, //22:00
+            {22, 30, R.raw.sound_2230, 0}  //22:30
+    };
+    private final static int msg_update = 0;
+    private static HandlerThread mHandlerThread;
+    private static WidgetHandler mHandler;
+    private static MediaPlayer mediaPlayer;
+    private static String timeStr = "";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         Log.i(TAG, "onUpdate");
-        this.appWidgetManager = appWidgetManager;
-        this.context = context;
-        sharedPreferences = context.getSharedPreferences(MainActivity.packageName, Context.MODE_PRIVATE);
+        NewAppWidget.appWidgetManager = appWidgetManager;
+        NewAppWidget.context = context;
         init();
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
+        long cur = System.currentTimeMillis();
+        BeiJingDate date = new BeiJingDate(cur);
+        updateText(date.getTimeStr());
         mHandler.removeMessages(msg_update);
         mHandler.sendEmptyMessageDelayed(msg_update, REFRESH_INTERVAL);
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        Log.i(TAG, "onDeleted");
+        super.onDeleted(context, appWidgetIds);
     }
 
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
         Log.i(TAG, "onEnabled");
-        this.context = context;
+        NewAppWidget.context = context;
         init();
-        mHandler.removeMessages(msg_update);
-        mHandler.sendEmptyMessage(msg_update);
     }
 
     @Override
@@ -78,8 +80,12 @@ public class NewAppWidget extends AppWidgetProvider {
             return;
         }
         isInit = true;
+        Log.i(TAG, "init");
         // Construct the RemoteViews object
         views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, requestCode, new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+        sharedPreferences = context.getSharedPreferences(MainActivity.packageName, Context.MODE_PRIVATE);
         mHandlerThread = new HandlerThread("WidgetHandlerThread");
         mHandlerThread.start();
         mHandler = new WidgetHandler(mHandlerThread.getLooper());
@@ -90,25 +96,17 @@ public class NewAppWidget extends AppWidgetProvider {
             return;
         }
         isInit = false;
+        Log.i(TAG, "release");
+        mHandlerThread.quit();
         mHandler.removeCallbacksAndMessages(null);
-        mHandlerThread.quitSafely();
+        timeStr = "";
+        if(mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 
-    private static int[][] soundId = new int[][]{
-            {20, 30, R.raw.sound_2030}, //20:30
-            {21, 00, R.raw.sound_2100}, //21:00
-            {21, 30, R.raw.sound_2130}, //21:30
-            {22, 00, R.raw.sound_2200}, //22:00
-            {22, 30, R.raw.sound_2230}  //22:30
-    };
-
-    private final static int msg_update = 0;
-    private final static String ALARM_TIME = "alarm_time";
-    private HandlerThread mHandlerThread;
-    private WidgetHandler mHandler;
-    private MediaPlayer mediaPlayer;
-    private long mHour, mMin;
-    private class WidgetHandler extends Handler {
+    private static class WidgetHandler extends Handler {
         public WidgetHandler(Looper looper) {
             super(looper);
         }
@@ -118,46 +116,26 @@ public class NewAppWidget extends AppWidgetProvider {
             switch (msg.what) {
                 case msg_update:
                     long cur = System.currentTimeMillis();
+                    //更新时间显示
                     BeiJingDate date = new BeiJingDate(cur);
-                    if(date.getHour() != mHour || date.getMin() != mMin) {
-                        mHour = date.getHour();
-                        mMin = date.getMin();
-                        updateText(date.getTimeStr());
+                    String str = date.getTimeStr();
+                    if(!timeStr.equals(str)) {
+                        updateText(str);
                     }
-                    long alarmTime = msg.getData().getLong(ALARM_TIME);
-                    long threshold = BeiJingDate.getLong(0, 3, 0, 0);
-                    if(alarmTime <= cur //时间到达
-                            && cur <= alarmTime + threshold //由于休眠导致时间过去太久了，或者alarmTime为0
-                            ) {
-                        playTipSound(alarmTime);
+                    //对比时间，播放提示
+                    int resId = checkTime(date);
+                    if(resId != 0) {
+                        playTipSound(resId);
                     }
-                    Message message = mHandler.obtainMessage(msg_update);
-                    Bundle bundle = new Bundle();
-                    bundle.putLong(ALARM_TIME, getAlarmTime(cur));
-                    message.setData(bundle);
+
                     mHandler.removeMessages(msg_update);
-                    mHandler.sendMessageDelayed(message, REFRESH_INTERVAL);
+                    mHandler.sendEmptyMessageDelayed(msg_update, REFRESH_INTERVAL);
                     break;
             }
         }
     }
 
-    private long getAlarmTime(long cur) {
-        BeiJingDate date = new BeiJingDate(cur);
-        long start = date.getTime(20, 30, 0, 0); //20:30
-        long end = date.getTime(22, 30, 0, 0); //22:30
-        long interval = BeiJingDate.getLong(0, 30, 0, 0);
-        long alarmTime = 0;
-        for(long time=start;time<=end;time+=interval) {
-            if(cur<time) {
-                alarmTime = time;
-                break;
-            }
-        }
-        return alarmTime;
-    }
-
-    private void updateText(String time) {
+    private static void updateText(String time) {
         if(context == null) {
             return;
         }
@@ -167,36 +145,43 @@ public class NewAppWidget extends AppWidgetProvider {
             ComponentName componentName = new ComponentName(context, NewAppWidget.class);
             // Instruct the widget manager to update the widget
             appWidgetManager.updateAppWidget(componentName, views);
+            timeStr = time;
         } catch (Exception e) {
             Log.e(TAG, "updateText fail "+e.getMessage());
         }
     }
 
-    private void playTipSound(long time) {
+    private static int checkTime(BeiJingDate date) {
+        int resId = 0;
+        long min = date.getMin();
+        long hour = date.getHour();
+        for(int[] item : soundId) {
+            if(item[0] == hour && item[1] == min) {
+                if(item[3] == 0) {
+                    resId = item[2];
+                    item[3] = 1;
+                    Log.i(TAG, "find " + hour + ":" + min + " " + resId);
+                }
+            }
+            else {
+                item[3] = 0;
+            }
+        }
+        return resId;
+    }
+
+    private static void playTipSound(int resId) {
         if(context == null) {
             return;
         }
         try {
-            if(mediaPlayer != null) {
-                mediaPlayer.release();
-            }
-            int resId = 0;
-            BeiJingDate date = new BeiJingDate(time);
-            long min = date.getMin();
-            long hour = date.getHour();
-            for(int[] item : soundId) {
-                if(item[0] == hour && item[1] == min) {
-                    resId = item[2];
-                    Log.i(TAG, "playTipSound "+hour+":"+min+" "+resId);
-                }
-            }
-            if(resId == 0) {
-                Log.e(TAG, "nuknow resId "+resId);
-                return;
-            }
             boolean alarm = sharedPreferences.getBoolean(MainActivity.ALARM, false);
             Log.i(TAG, "alarm setting : "+alarm);
             if(alarm) {
+                if(mediaPlayer != null) {
+                    mediaPlayer.release();
+                    mediaPlayer = null;
+                }
                 mediaPlayer = MediaPlayer.create(context, resId);
                 mediaPlayer.start();
             }
